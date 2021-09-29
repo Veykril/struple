d513c9886a2df193d7e597d8a02419560d17d382
TATSUNO Yasuhiro <ytatsuno.jp@gmail.com>
2021-09-29 17:15:55 +0900

    Add round-trip test

--- tests/derive_success.rs
+++ tests/derive_success.rs
@@ -142,6 +142,22 @@ fn test_unary_impl() {
 }
 
 #[test]
+fn test_huge_impl() {
+    let foo = Huge::from_tuple((
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25,
+        26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48,
+        49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71,
+        72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94,
+        95, 96, 97, 98, 99, 100,
+    ));
+    let bar = foo.into_tuple();
+
+    // Note) Tuple does not implement PartialEq on Tuples whose arity is greater than 12
+    assert_eq!(bar.0, 1);
+    assert_eq!(bar.99, 100);
+}
+
+#[test]
 fn test_named_impl() {
     let foo = Named::from_tuple((3, 5, "bar".into()));
     assert_eq!(foo.into_tuple(), (3, 5, "bar".into()));
